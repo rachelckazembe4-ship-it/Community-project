06a05055774d826ca302e0c28eed53fd0962eef7
rachelckazembe4-ship-it <rachelckazembe4@gmail.com>
2026-07-01 19:02:44 +0300

    Update deployment config

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -72,7 +72,7 @@ TIME_ZONE = 'Africa/Dar_es_Salaam'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'dashboard' / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
